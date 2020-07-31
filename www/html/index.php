b64209326d484118b73d62a667c05109f44e4f4a
kensuke0518 <kensuke0518act@gmail.com>
2020-08-01 00:29:28 +0900

    PDOと例外処理に対応

--- www/html/index.php
+++ www/html/index.php
@@ -1,8 +1,16 @@
 <?php
-$dsn = 'mysql:host=db:3306;dbname=';  //dbname=自分で作成したデータベーススペース名（database space name）を入れる
+$dsn = 'mysql:host=db:3306;dbname=データベーススペース名';    //dbname=自分で作成したデータベーススペース名（database space name）を入れる
 $user = 'root';
 $pass = 'secret';
 
-$dbn = new PDO($dsn,$user,$pass);
-echo 'データベースに接続しました';
+try{
+    $dbh = new PDO($dsn,$user,$pass);
+    $dbh->query("set names utf8");
+    echo 'データベースに接続しました';
+    //以下に命令を記述
+
+} catch(PDOException $e){
+    echo $e->getMessage();
+    die();
+}
 ?>
